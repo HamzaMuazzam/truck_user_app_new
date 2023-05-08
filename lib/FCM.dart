ecee2f1552d9ebb0e0f7c22c216ed945e352636f
HamzaMuazzam <57168472+HamzaMuazzam@users.noreply.github.com>
2023-05-08 19:11:38 +0500

    null error handle for web

--- lib/FCM.dart
+++ lib/FCM.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sultan_cab/services/ApiServices/StorageServices/get_storage.dart';
 import 'notification_service.dart';
 int notificationNumber = 0;
@@ -17,53 +18,64 @@ class FCM {
   FirebaseMessaging? _messaging;
 
   FCM() {
-    FlutterLocalNotificationsPlugin fltrNotification;
-    var androidInitilize = const AndroidInitializationSettings('logo');
-    var iOSinitilize = const DarwinInitializationSettings();
-    var initilizationsSettings = InitializationSettings(android: androidInitilize, iOS: iOSinitilize);
-    var androidDetails = const AndroidNotificationDetails("TRUC-KING", "HAMZA "
-        "MUAZZAM programmer", importance: Importance.max, priority: Priority.max);
-    var iSODetails = const DarwinNotificationDetails();
-    var generalNotificationDetails = NotificationDetails(android: androidDetails, iOS: iSODetails);
+    try{
+      FlutterLocalNotificationsPlugin fltrNotification;
+      var androidInitilize = const AndroidInitializationSettings('logo');
+      var iOSinitilize = const DarwinInitializationSettings();
+      var initilizationsSettings =
+          InitializationSettings(android: androidInitilize, iOS: iOSinitilize);
+      var androidDetails = const AndroidNotificationDetails(
+          "TRUC-KING",
+          "HAMZA "
+              "MUAZZAM programmer",
+          importance: Importance.max,
+          priority: Priority.max);
+      var iSODetails = const DarwinNotificationDetails();
+      var generalNotificationDetails =
+          NotificationDetails(android: androidDetails, iOS: iSODetails);
 
-    fltrNotification = FlutterLocalNotificationsPlugin();
-    Firebase.initializeApp().then((fbr) async {
-      _messaging = FirebaseMessaging.instance;
-      _messaging!.getToken().then((token) async {
-        FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+      fltrNotification = FlutterLocalNotificationsPlugin();
+      Firebase.initializeApp().then((fbr) async {
+        _messaging = FirebaseMessaging.instance;
+        _messaging!.getToken().then((token) async {
+          FirebaseMessaging.onBackgroundMessage(
+              _firebaseMessagingBackgroundHandler);
 
-        await _messaging!.subscribeToTopic("AllUsers").then((value) => null);
-        if (StorageCRUD.getUser().id!=null) {
-          await _messaging!.subscribeToTopic("${StorageCRUD.getUser().id}")
-              .then((value) => null);
-        }
+          await _messaging!.subscribeToTopic("AllUsers").then((value) => null);
+          if (StorageCRUD.getUser().id != null) {
+            await _messaging!
+                .subscribeToTopic("${StorageCRUD.getUser().id}")
+                .then((value) => null);
+          }
 
-        NotificationSettings settings = await _messaging!.requestPermission(
-          alert: true,
-          badge: true,
-          provisional: false,
-          sound: true,
-        );
+          NotificationSettings settings = await _messaging!.requestPermission(
+            alert: true,
+            badge: true,
+            provisional: false,
+            sound: true,
+          );
 
-        if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-          await Firebase.initializeApp();
-          FirebaseMessaging.onMessageOpenedApp.listen((event) {
-            NotificationRoutes.onNotificationClick(event.data);
-          });
-          FirebaseMessaging.onMessage.listen((event) async {
-            print('FirebaseMessaging.onMessage.listen');
-            print("event.data ${event.data.toString()}");
+          if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+            await Firebase.initializeApp();
+            FirebaseMessaging.onMessageOpenedApp.listen((event) {
+              NotificationRoutes.onNotificationClick(event.data);
+            });
+            FirebaseMessaging.onMessage.listen((event) async {
+              print('FirebaseMessaging.onMessage.listen');
+              print("event.data ${event.data.toString()}");
 
-            NotificationRoutes.onMessageRecived(
-                event,
-                isDataFromBackground: false,
-                fltrNotification: fltrNotification,
-                initilizationsSettings: initilizationsSettings,
-                generalNotificationDetails:generalNotificationDetails
-            );
-          });
-        }
+              NotificationRoutes.onMessageRecived(event,
+                  isDataFromBackground: false,
+                  fltrNotification: fltrNotification,
+                  initilizationsSettings: initilizationsSettings,
+                  generalNotificationDetails: generalNotificationDetails);
+            });
+          }
+        });
       });
-    });
+    }catch(e){
+      print(e);
+      Fluttertoast.showToast(msg: e.toString());
+    }
   }
 }
--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:sultan_cab/services/ApiServices/api_services.dart';
 
@@ -28,50 +29,64 @@ class NotificationRoutes {
       FlutterLocalNotificationsPlugin? fltrNotification,
       InitializationSettings? initilizationsSettings,
       NotificationDetails? generalNotificationDetails}) async {
-    ///TODO: this check will insure that if app is open and in foreground and
-    ///we need to show notification or not
-    ///TODO: like if chat screen is open and message come for the same chat user so here we will prevent to not to show notification
-    ///TODO: if(!isDataFromBackground && chatNextUserId==event.data['nextUserIs']) { return don't  show notification here.}. ==> {HAMZA MUAZZAM 01-07-2022}
+    try{
+      ///TODO: this check will insure that if app is open and in foreground and
+      ///we need to show notification or not
+      ///TODO: like if chat screen is open and message come for the same chat user so here we will prevent to not to show notification
+      ///TODO: if(!isDataFromBackground && chatNextUserId==event.data['nextUserIs']) { return don't  show notification here.}. ==> {HAMZA MUAZZAM 01-07-2022}
 
-    if (!isDataFromBackground!) {
-      fltrNotification!.initialize(initilizationsSettings!,
-          onDidReceiveNotificationResponse:
-              (NotificationResponse notificationResponse) async {
-        NotificationRoutes.onNotificationClick(event.data);
-      }, onDidReceiveBackgroundNotificationResponse: backGroundListen);
-      await fltrNotification.show(
-          notificationNumber,
-          event.notification != null ? event.notification!.title : "".tr,
-          event.notification != null ? event.notification!.body : "".tr,
-          generalNotificationDetails,
-          payload: event.data.toString());
-      ++notificationNumber;
+      if (!isDataFromBackground!) {
+        fltrNotification!.initialize(initilizationsSettings!,
+            onDidReceiveNotificationResponse:
+                (NotificationResponse notificationResponse) async {
+          NotificationRoutes.onNotificationClick(event.data);
+        }, onDidReceiveBackgroundNotificationResponse: backGroundListen);
+        await fltrNotification.show(
+            notificationNumber,
+            event.notification != null ? event.notification!.title : "".tr,
+            event.notification != null ? event.notification!.body : "".tr,
+            generalNotificationDetails,
+            payload: event.data.toString());
+        ++notificationNumber;
+      }
+    }
+    catch(e){
+      print(e);
+      Fluttertoast.showToast(msg: e.toString());
+
     }
   }
 
   //TODO: when user click on notification in  foreground or background.
   static onNotificationClick(Map<String, dynamic> payload) async {
-    logger.e(payload);
+    try{
+      logger.e(payload);
 
-    // String? screen = payload['Screen'];
-    //TODO: if app is not open so we have to wait here to let the app open... if context is null we will not go further. wait instead .
-    while (Get.context == null) {}
+      // String? screen = payload['Screen'];
+      //TODO: if app is not open so we have to wait here to let the app open... if context is null we will not go further. wait instead .
+      while (Get.context == null) {}
 
-    ///  maryam do work here if payload has specific value then go to that screen accordingly
-    ///  like payload['screenName']=="Order Details Screen" or   payload['orderId']!=null then go to order details screen with id
+      ///  maryam do work here if payload has specific value then go to that screen accordingly
+      ///  like payload['screenName']=="Order Details Screen" or   payload['orderId']!=null then go to order details screen with id
 
-    if (payload['Route'] == "order_screen" &&
-        (payload['orderId'] != null || payload['OrderId'] != null)) {
-      String? orderID = payload['OrderId'] ?? "";
-      String response = await ApiServices.getMethod(
-          feedUrl: "/Order/get-order-by-Id?id=$orderID");
+      if (payload['Route'] == "order_screen" &&
+          (payload['orderId'] != null || payload['OrderId'] != null)) {
+        String? orderID = payload['OrderId'] ?? "";
+        String response = await ApiServices.getMethod(
+            feedUrl: "/Order/get-order-by-Id?id=$orderID");
 
-      if (response.isNotEmpty) {
-        if (Get.currentRoute.contains("OrderDetailById")) {
-          Get.back();
+        if (response.isNotEmpty) {
+          if (Get.currentRoute.contains("OrderDetailById")) {
+            Get.back();
+          }
+          Get.to(OrderDetailById(
+              GetAllOrdersResponse.fromJson(json.decode(response))));
         }
-        Get.to(OrderDetailById(GetAllOrdersResponse.fromJson(json.decode(response))));
       }
+    }catch(e){
+      print(e);
+      Fluttertoast.showToast(msg: e.toString());
+
     }
   }
 }
