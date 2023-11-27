35cab102af8ff38c9495119f01a8f2214ef6bcc1
anasmufti97 <muftianas97@gmail.com>
2023-11-27 20:07:05 +0500

    map working

--- lib/utils/api_keys.dart
+++ lib/utils/api_keys.dart
@@ -3,7 +3,7 @@ import 'package:get_storage/get_storage.dart';
 /// TH
 // const String GoogleMapApiKey = 'AIzaSyCqcZ7xj1BPJeC3Uyo2coGi9qaGNpyU_EA';
 //
-const String GoogleMapApiKey = 'AIzaSyApyNQ6jz97TRRB7DrclHzXxfxm3VHs3Z8';
+const String GoogleMapApiKey = 'AIzaSyCDj6L424YNh9KEwxJXDedzc49D_BMicuE';
 const String RazorPayID = 'YOUR_PAY_ID_HERE';
 const String RazorPaySecretID = 'YOUR_SECRET_KEY_HERE';
 
